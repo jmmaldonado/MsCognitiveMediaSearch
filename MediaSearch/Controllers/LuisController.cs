7b0c5bfc7d9b7fe68b3b7422977fb37ab867878a
Jose Manuel Maldonado <jmmaldonado@outlook.com>
2016-04-09 23:37:06 +0200

    IndexWebJob: Run frequency changed to be compatible with basic tier webapps - now is set to 1 hour
    MediaServices:
    Random tests in the Luis section to read from storage accounts, laying the foundation for the LUIS training capabilities.

--- MediaSearch/Controllers/LuisController.cs
+++ MediaSearch/Controllers/LuisController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace MediaSearch.Controllers
 {
@@ -12,6 +15,8 @@ namespace MediaSearch.Controllers
         // GET: Luis
         public ActionResult Index()
         {
+            ViewBag.LUISAppId = ConfigurationManager.AppSettings["LUISAppId"];
+            ViewBag.BlobList = getTrainingBlobsFromStorageAccount();
             return View();
         }
 
@@ -22,5 +27,44 @@ namespace MediaSearch.Controllers
 
             return "Guardia quiere comer ya!";
         }
+
+
+
+
+
+        public List<Uri> getTrainingBlobsFromStorageAccount() {
+            List<Uri> result = new List<Uri>();
+
+            string storageAccountName = "storagecallcenterdemo";
+            string storageAccountKey = "5r0hNiuYnAx8Dn3GLAm3mpxVNNPMUfOobecQ+teghHKh5ryJN6Xyc2itasjaGSrR2LjIDyedArFC2GngpG3IzQ==";
+
+            string storageAccountConnectionString = "DefaultEndpointsProtocol=https;AccountName=" + storageAccountName + ";AccountKey=" + storageAccountKey;
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageAccountConnectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            IEnumerable<CloudBlobContainer> containerList = blobClient.ListContainers();
+
+
+            //Listado de todos los contenedores
+            foreach (CloudBlobContainer container in containerList) {
+
+                //Listado de todos los blobs dentro del contenedor actual
+                IEnumerable<IListBlobItem> blobList = container.ListBlobs();
+
+                foreach (IListBlobItem blob in blobList) {
+
+                    string filename = blob.Uri.Segments.Last();
+                    string[] filenameArray = filename.Split('.');
+
+                    result.Add(blob.Uri);
+
+                }
+
+            }
+
+
+            return result;
+        }
+
+
     }
 }
